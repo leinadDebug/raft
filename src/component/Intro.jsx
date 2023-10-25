f7e4d5901a194b5331678ef29535cf2612f1ae8f
leinadDebug <akujuaobidaniel@gmail.com>
2023-10-25 14:28:23 +0100

    -mobile changes on intro.jsx

--- src/component/Intro.jsx
+++ src/component/Intro.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Mobileimg from '../assets/Frame 31.png';
+import React from "react";
+import Mobileimg from "../assets/Frame 31.png";
 import Desktopimg from "../assets/Frame 31 (1).png";
-import bloomberg from '../assets/png/bloomberg-1.png'
-import cnbc from '../assets/png/cnbc-1.png'
-import cnn from '../assets/png/cnn-logo 1.png'
-import reuters from '../assets/png/reuters-2-1 1.png'
+import bloomberg from "../assets/png/bloomberg-1.png";
+import cnbc from "../assets/png/cnbc-1.png";
+import cnn from "../assets/png/cnn-logo 1.png";
+import reuters from "../assets/png/reuters-2-1 1.png";
 import vector from "../assets/png/Vector.png";
 
 function Intro() {
-  const title = 'Introducing Raft Cards >'
+  const title = "Introducing Raft Cards >";
   return (
-    <div className="bg-[#070606] text-white md:mx-10 lg:mx-20 mx-10 py-20 px-10 text-center">
-      <button className="bg-[#4d4a4a] p-2 px-3 rounded-full">{title}</button>
-      <h1 className=" text-3xl md:text-6xl my-5 p-2 lg:text-8xl lg:w-[50%] m-auto">
+    <div className="bg-[#070606] md:mx-10 lg:mx-20  py-20 px-10 text-white text-center">
+      <button className="bg-[#4d4a4a] py-2 px-3 rounded-full ">{title}</button>
+      <h1 className="my-5 p-2 md:text-6xl lg:text-8xl lg:w-[50%] m-auto text-4xl">
         Building the future of banking.
       </h1>
-      <p className=" p-2 px-5 lg:my-6 lg:text-2xl lg:w-[50%] m-auto">
+      <p className=" p-2 px-5 lg:my-6 lg:text-2xl lg:w-[50%] m-auto text-[#989898] mb-4">
         Experience the future of banking with RAFT. We're here to empower your
         financial journey.
       </p>
@@ -27,13 +27,13 @@ function Intro() {
         <img className="hidden lg:flex" src={Desktopimg} />
       </div>
       <div className="lg:mt-16 lg:text-2xl mt-6  text-[#BDBDBD]">
-        <h2 className="lg:mb-14">FEATURED AND SEEN IN</h2>
-        <div className='flex gap-16 justify-center items-center '>
-          <img className='lg:h-9 ' src={vector} />
-          <img className='lg:h-14 ' src={cnbc} />
-          <img className='lg:h-14 ' src={bloomberg} />
-          <img className='lg:h-10 ' src={reuters} />
-          <img className='lg:h-10 ' src={cnn} />
+        <h2 className="lg:mb-14 mb-7">FEATURED AND SEEN IN</h2>
+        <div className="flex lg:gap-16 gap-6 justify-center items-center lg:overflow-x-none overflow-x-auto ">
+          <img className="lg:h-9 h-9 " src={vector} />
+          <img className="lg:h-14 h-9" src={cnbc} />
+          <img className="lg:h-14 h-9" src={bloomberg} />
+          <img className="lg:h-10 h-9" src={reuters} />
+          <img className="lg:h-10 h-9" src={cnn} />
           <div></div>
         </div>
       </div>
@@ -41,4 +41,4 @@ function Intro() {
   );
 }
 
-export default Intro
+export default Intro;
