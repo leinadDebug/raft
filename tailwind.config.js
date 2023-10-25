8f6619417045e4958e139e9241c6fa723d861f2e
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-10-25 13:44:48 +0100

    added SF pro display font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily:{
+      sfProDisplay : ['SF Pro Display', "sans-serif"]
+    }
   },
   plugins: [],
 };
