e5cd4186c69f105a5f8ca8139826823794a1fcb5
leinadDebug <akujuaobidaniel@gmail.com>
2023-10-27 20:02:14 +0100

    --added intro.jsx

--- src/component/Intro.jsx
+++ src/component/Intro.jsx
@@ -12,7 +12,7 @@ function Intro() {
   return (
     <div className="bg-[#070606] md:mx-10 lg:mx-20  py-20 px-10 text-white text-center">
       <button className="bg-[#4d4a4a] py-2 px-3 rounded-full ">{title}</button>
-      <h1 className="my-5 p-2 md:text-6xl lg:text-8xl lg:w-[50%] m-auto text-4xl">
+      <h1 className="my-5 p-2 md:text-6xl lg:text-8xl lg:w-[50%] m-auto text-4xl tracking-wide">
         Building the future of banking.
       </h1>
       <p className=" p-2 px-5 lg:my-6 lg:text-2xl lg:w-[50%] m-auto text-[#989898] mb-4">
@@ -23,8 +23,14 @@ function Intro() {
         Get Started
       </button>
       <div className=" p-3 flex justify-center mt-10 lg:mt-32 ">
-        <img className="lg:hidden" src={Mobileimg} />
-        <img className="hidden lg:flex" src={Desktopimg} />
+        <img
+          className="lg:hidden border-2 border-green-400 rounded-lg"
+          src={Mobileimg}
+        />
+        <img
+          className="hidden lg:flex border-2 border-green-400 rounded-2xl"
+          src={Desktopimg}
+        />
       </div>
       <div className="lg:mt-16 lg:text-2xl mt-6  text-[#BDBDBD]">
         <h2 className="lg:mb-14 mb-7">FEATURED AND SEEN IN</h2>
@@ -35,6 +41,16 @@ function Intro() {
           <img className="lg:h-10 h-9" src={reuters} />
           <img className="lg:h-10 h-9" src={cnn} />
           <div></div>
+        </div>
+        <div className="lg:mt-52 mt-16 ">
+          <h1 className="my-5 p-2 md:text-5xl lg:text-8xl lg:w-[60%] m-auto text-4xl text-white tracking-wide">
+            Elevate Your Financial Journey with RAFT
+          </h1>
+          <p className=" p-2 px-5 lg:my-6 lg:text-2xl lg:w-[50%] m-auto text-[#989898] mb-4">
+            RAFT offers a world of possibilities. From investment to payments,
+            we've got you covered. Join us and unlock your financial potential
+            today.
+          </p>
         </div>
       </div>
     </div>
