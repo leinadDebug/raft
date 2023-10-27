91270921e198cf5f90f1a0125ea12afb40c36d7f
leinadDebug <akujuaobidaniel@gmail.com>
2023-10-27 20:04:34 +0100

    --updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
+import Confidently from './component/Confidently'
+import FAQ from './component/FAQ'
 import Financial from './component/Financial'
+import GenCards from './component/GenCards'
+import Section from './component/Section'
+import Stats from './component/Stats'
 import Intro from './component/intro'
 import NavBar from './component/navbar'
 
@@ -9,10 +14,15 @@ function App() {
   return (
     <div className="font-sfProDisplay font-normal">
       <NavBar />
-      <Intro/>
-      <Financial/>
+      <Intro />
+      <Section />
+      <Financial />
+      <Confidently />
+      <Stats/>
+      <GenCards/>
+      <FAQ/>
     </div>
-  )
+  );
 }
 
 export default App
